2d0a137caad7e684b54470a8dc99190c7780c9d7
a$ong <fwscool@163.com>
2025-10-21 02:05:55 +0800

    2-5 dash

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,33 @@
 extends Unit
 class_name Player
 
+@export var dash_duration := 0.3
+@export var dash_speed_multi := 2.5
+@export var dash_cooldown := 1.5
+
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_cooldown_timer: Timer = $DashCooldownTimer
+@onready var collision: CollisionShape2D = $CollisionShape2D
+
 var move_dir: Vector2
+var is_dashing := false
+var dash_available := true
+
+func _ready() -> void:
+	dash_timer.wait_time = dash_duration
+	dash_cooldown_timer.wait_time = dash_cooldown
 
 func _process(delta: float) -> void:
 	move_dir = Input.get_vector("move_left","move_right","move_up","move_down")
 	
 	var current_velocity := move_dir * 500
+	if is_dashing:
+		current_velocity *= dash_speed_multi
+		
 	position += current_velocity * delta
+	
+	if can_dash():
+		start_dash()
 	
 	update_animations()
 	update_rotation()
@@ -26,3 +46,24 @@ func update_rotation()-> void:
 		visuals.scale = Vector2(1.0,1.0)
 	else:
 		visuals.scale = Vector2(-1.0,1.0)
+
+func start_dash() -> void:
+	is_dashing = true
+	dash_timer.start()
+	visuals.modulate.a = 0.5
+	collision.set_deferred("disabled",true)
+	
+func can_dash() -> bool:
+	return not is_dashing and\
+	dash_cooldown_timer.is_stopped() and\
+	Input.is_action_just_pressed("dash") and\
+	move_dir != Vector2.ZERO
+	
+
+
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
+	visuals.modulate.a = 1.0
+	move_dir = Vector2.ZERO
+	collision.set_deferred("disabled",false)   
+	dash_cooldown_timer.start()
